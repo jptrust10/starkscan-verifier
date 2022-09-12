d564a4905834c4bcd6468e524b7778bd4daf668a
0xs34n <fluffy@diamondpaws.xyz>
2022-09-12 17:08:05 -0400

    small fixes

--- src/steps/getRootCairoFile.ts
+++ src/steps/getRootCairoFile.ts
@@ -1,48 +1,48 @@
 import { isString } from "class-validator";
-import inquirer from "inquirer"
-
+import inquirer from "inquirer";
 
 async function validateRootCairoFile(input: any): Promise<string | boolean> {
-  const inputValue = input["value"]
+  const inputValue = input["value"];
   if (!isString(inputValue)) {
-    return "must be a string"
+    return "must be a string";
   }
 
   if (!inputValue.endsWith(".cairo")) {
-    return "must be a cairo file"
+    return "must be a cairo file";
   }
-  
+
   return true;
 }
 
 export async function getRootCairoFile(): Promise<string> {
   // https://github.com/adelsz/inquirer-fuzzy-path
-  inquirer.registerPrompt("fuzzypath", require("inquirer-fuzzy-path"))
+  inquirer.registerPrompt("fuzzypath", require("inquirer-fuzzy-path"));
 
-  const userInput = await inquirer.prompt(
-    {
-      // @ts-ignore
-      type: "fuzzypath",
-      name: "RootCairoFile",
-      message: "Cairo File Name: ",
-      itemType: "file",
-      suggestOnly: false,
-      excludePath: (nodePath: string) => {
-        return (
-          // potential python envs
-          nodePath.startsWith("venv") || nodePath.startsWith("env") ||
-          nodePath.startsWith(".venv") || nodePath.startsWith(".env") ||
-          // javascript modules
-          nodePath.startsWith("node_modules")
-        )
-      },
-      excludeFilter: (nodePath: string) => {
-        return !nodePath.endsWith(".cairo")
-      },
-      validate(input: string) {
-        return validateRootCairoFile(input)
-      }
-    }
-  )
-  return userInput.RootCairoFile.trim()
+  const userInput = await inquirer.prompt({
+    // @ts-ignore
+    type: "fuzzypath",
+    name: "RootCairoFile",
+    message: "Cairo File Name: ",
+    itemType: "file",
+    suggestOnly: true,
+    excludePath: (nodePath: string) => {
+      return (
+        // potential python envs
+        nodePath.startsWith("venv") ||
+        nodePath.startsWith("env") ||
+        nodePath.startsWith(".venv") ||
+        nodePath.startsWith(".env") ||
+        // javascript modules
+        nodePath.startsWith("node_modules")
+      );
+    },
+    excludeFilter: (nodePath: string) => {
+      return !nodePath.endsWith(".cairo");
+    },
+    // TODO jkoh
+    // validate(input: string) {
+    //   return validateRootCairoFile(input);
+    // },
+  });
+  return userInput.RootCairoFile.trim();
 }
