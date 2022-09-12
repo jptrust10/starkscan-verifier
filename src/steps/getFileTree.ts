0c567e17cd713d44aaced4a04255c5eac61d9697
0xmonkeyy <monkey@diamondpaws.xyz>
2022-09-12 16:48:09 -0400

    add file tree logic

--- src/steps/getFileTree.ts
+++ src/steps/getFileTree.ts
@@ -4,6 +4,7 @@ import inquirer from "inquirer"
 import { Files } from "types"
 
 const IMPORT_REGEX = /^from\s(.*?)\simport/gm
+const ui = new inquirer.ui.BottomBar();
 
 export class CannotFindFileError extends Error {
   constructor(message: string) {
@@ -15,33 +16,24 @@ export async function getFileTree(rootFilePath: string): Promise<Files> {
   // default to include path of root file
   const rootFileDirectory = path.dirname(rootFilePath)
 
-  const userInput = await inquirer.prompt(
-    {
-      type: "input",
-      name: "CairoPath",
-      message: "Cairo Path: ",
-    },
-  )
-  const searchPath = userInput.CairoPath.trim()
-  const searchPaths = [rootFileDirectory, searchPath]
-  const files = buildFileTree(path.basename(rootFilePath), searchPaths)
+  const files = await buildFileTree(path.basename(rootFilePath), [rootFileDirectory])
   return files
 }
 
 
-function buildFileTree(filePath: string, searchPaths: string[]) {
+async function buildFileTree(filePath: string, searchPaths: string[]) {
   const files: Files = {}
-  _buildFileTree(files, filePath, searchPaths);
+  await _buildFileTree(files, filePath, searchPaths);
   return files
 }
 
 
-function _buildFileTree(files: Files, filePath: string, searchPaths: string[]) {
+async function _buildFileTree(files: Files, filePath: string, searchPaths: string[]) {
   if (filePath in files) {
     return files;
   }
 
-  const fileValue = getFileContents(filePath, searchPaths)
+  const fileValue = await getFileContents(filePath, searchPaths)
   files[filePath] = fileValue
   const importedFilesRegexOutput = [...fileValue.matchAll(IMPORT_REGEX)]
   const importedFilesPath = importedFilesRegexOutput.map(importedFileRegex => {
@@ -55,16 +47,17 @@ function _buildFileTree(files: Files, filePath: string, searchPaths: string[]) {
       continue
     }
     const convertedFilePath = importedFilePath.split(".").join("/") + ".cairo"
-    _buildFileTree(files, convertedFilePath, searchPaths)
+    await _buildFileTree(files, convertedFilePath, searchPaths)
   }
 
   return files;
 }
 
 // returns contents of the file
-function getFileContents(filePath: string, searchPaths: string[]): string {
+async function getFileContents(filePath: string, searchPaths: string[]): Promise<string> {
   const fileExists = fs.existsSync(filePath)
   if (fileExists) {
+    ui.log.write(`FOUND FILE!: ${filePath}`)
     return fs.readFileSync(filePath, "utf-8")
   }
 
@@ -73,9 +66,45 @@ function getFileContents(filePath: string, searchPaths: string[]): string {
     const potentialFullFilePath = path.join(searchPath, filePath)
     const fileExists = fs.existsSync(potentialFullFilePath)
     if (fileExists) {
+      ui.log.write(`FOUND FILE!: ${potentialFullFilePath}`)
       return fs.readFileSync(potentialFullFilePath, "utf-8")
     }
   }
 
-  throw new CannotFindFileError(`Could not find file: ${filePath}`)
+  ui.log.write(`cannot find file: ${filePath}`)
+  const userInput = await inquirer.prompt(
+    {
+      // @ts-ignore
+      type: "fuzzypath",
+      name: "CairoFile",
+      message: "Cairo File: ",
+      itemType: "file",
+      suggestOnly: true,
+      excludePath: (nodePath: string) => {
+        return (
+          // potential python envs
+          // nodePath.startsWith("venv") || nodePath.startsWith("env") ||
+          // nodePath.startsWith(".venv") || nodePath.startsWith(".env") ||
+          // javascript modules
+          nodePath.startsWith("node_modules")
+        )
+      },
+      excludeFilter: (nodePath: string) => {
+        return !nodePath.endsWith(".cairo")
+      },
+      // TODO validate
+      // validate(input: string) {
+      //   return validateRootCairoFile(input)
+      // }
+    }
+  )
+  // console.log("AFTER!", userInput)
+  // throw new Error("WTF")
+
+  const cairoFile = userInput.CairoFile.trim()
+  ui.log.write(`FOUND FILE!: ${cairoFile}`)
+  return fs.readFileSync(cairoFile, "utf-8")
+
+  // // return userInput.CairoFile.trim()
+  // throw new CannotFindFileError(`Could not find file: ${filePath}`)
 }
